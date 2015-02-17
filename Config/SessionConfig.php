8e72e1872cbcb826a91ca651b7615bbd63707bdc
Matthew Weier O'Phinney <matthew@zend.com>
2015-02-17 19:56:04 +0000

    Importing state as of zendframework/zf2@a24f638c30145cdcc58658085bd1ad6911012fe8 (1424202964)
    Automatic import via rsync

--- Config/SessionConfig.php
+++ Config/SessionConfig.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -42,7 +42,6 @@ class SessionConfig extends StandardConfig
      * @var array Valid cache limiters (per session.cache_limiter)
      */
     protected $validCacheLimiters = array(
-        '',
         'nocache',
         'public',
         'private',
@@ -73,6 +72,7 @@ class SessionConfig extends StandardConfig
      */
     public function setStorageOption($storageName, $storageValue)
     {
+        $key = false;
         switch ($storageName) {
             case 'remember_me_seconds':
                 // do nothing; not an INI option
@@ -86,10 +86,9 @@ class SessionConfig extends StandardConfig
         }
 
         $result = ini_set($key, $storageValue);
-        if (false === $result) {
-            throw new Exception\InvalidArgumentException(
-                "'{$key}' is not a valid sessions-related ini setting."
-            );
+        if (FALSE === $result) {
+            throw new Exception\InvalidArgumentException("'" . $key .
+                    "' is not a valid sessions-related ini setting.");
         }
         return $this;
     }
@@ -161,6 +160,7 @@ class SessionConfig extends StandardConfig
         $this->setOption('save_path', $savePath);
         return $this;
     }
+
 
     /**
      * Set session.serialize_handler
--- Container.php
+++ Container.php
@@ -3,11 +3,17 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
 namespace Zend\Session;
+
+if (version_compare(PHP_VERSION, '5.3.4', 'lt')) {
+    class_alias('Zend\Session\AbstractContainer', 'Zend\Session\AbstractBaseContainer');
+} else {
+    class_alias('Zend\Session\Container\PhpReferenceCompatibility', 'Zend\Session\AbstractBaseContainer');
+}
 
 /**
  * Session storage container
@@ -17,24 +23,6 @@ namespace Zend\Session;
  * Additionally, expiries may be absolute TTLs or measured in "hops", which
  * are based on how many times the key or container were accessed.
  */
-class Container extends AbstractContainer
+class Container extends AbstractBaseContainer
 {
-    /**
-     * Retrieve a specific key in the container
-     *
-     * @param  string $key
-     * @return mixed
-     */
-    public function &offsetGet($key)
-    {
-        $ret = null;
-        if (!$this->offsetExists($key)) {
-            return $ret;
-        }
-        $storage = $this->getStorage();
-        $name    = $this->getName();
-        $ret =& $storage[$name][$key];
-
-        return $ret;
-    }
 }
